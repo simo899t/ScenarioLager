a6a659743bfbde88775062da2de230b5fcbabce4
nomis <simon@cogito.dk>
2025-12-24 12:02:42 +0100

    Update scenario-lager.php

--- wordpress-plugin/scenario-lager.php
+++ wordpress-plugin/scenario-lager.php
@@ -3,7 +3,7 @@
  * Plugin Name: Scenario Lager
  * Plugin URI: http://multipartner.dk/lager
  * Description: Equipment checkout and storage management system for Scenario
- * Version: 2.2.4
+ * Version: 2.2.5
  * Author: Simon Holm
  * Author URI: https://multipartner.dk
  * License: GPL v2 or later
@@ -29,7 +29,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('SCENARIO_LAGER_VERSION', '2.2.4');
+define('SCENARIO_LAGER_VERSION', '2.2.5');
 define('SCENARIO_LAGER_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('SCENARIO_LAGER_PLUGIN_URL', plugin_dir_url(__FILE__));
 
